af3161f767f5c798c2813695452b6e1800d76348
lsj913 <tnwlsdl1113@naver.com>
2021-05-25 00:08:59 +0900

    upload file

---
+++ shiny.R
@@ -0,0 +1,50 @@
+library(shiny)
+library(ggplot)
+library(data.table)
+
+
+ui <- fluidPage(
+  titlePanel("The number of film"),
+
+  sidebarLayout(
+    sidebarPanel(
+      helpText('Creat the number of release film or film in boxoffice per year(2019 ~ 2020)')
+    selectInput('var',
+                 label = 'choose a case to display',
+                 choices = c('release film' , 'film in boxoffice', 'unique film in boxoffice'),
+                 selected = 'release film')
+    ),
+    
+    mainPanel(plotOutput('N_film'))
+  )
+)
+
+
+server <- function(input, output) {
+  output$N_film <- renderPlot({
+    data <- switch(input$var,
+                   'release film' = release,
+                   'film in boxoffice' = release_type,
+                   'unique film in boxoffice' = release_type_unique)
+    X <- switch(input$var,
+                   'release film' = release$year,
+                   'film in boxoffice' = release_type$year,
+                   'unique film in boxoffice' = release_type_unique$year)
+    Y <- switch(input$var,
+                    'release film' = release$N,
+                    'film in boxoffice' = release_type$N,
+                    'unique film in boxoffice' = release_type_unique$N)
+    Fill <- switch(input$var,
+                   'release film' = release$type,
+                   'film in boxoffice' = release_type$type,
+                   'unique film in boxoffice' = release_type_unique$type)
+    data %>% ggplot(mapping = aes(x = X, y = Y, fill = Fill)) + geom_bar(stat = 'identity', position = 'dodge') + 
+      scale_fill_brewer(palette = "Pastel1")+ ylim(0, 140) +
+      geom_text(aes(label = Y), vjust = 1.5, colour = "black", position = position_dodge(1), size = 3) +
+      theme(plot.title = element_text(size = 15), axis.title = element_text(size = 10), axis.text.x = element_text(size = 10))
+  })
+  }
+  
+
+shinyApp(ui = ui, server = server)
+
