ba947781eb8739dbe36ac270ab23dbd7b1f929d4
lsj913 <66099242+lsj913@users.noreply.github.com>
2021-06-02 02:30:17 +0900

    Update app.R

--- app.R
+++ app.R
@@ -14,12 +14,13 @@ ui <- fluidPage(
     selectInput('var',
                  label = 'choose a case to display',
                  choices = c('release film' , 'film in boxoffice', 'unique film in boxoffice'),
-                 selected = 'release film')
-    ),
+                 selected = 'release film'),
+    checkboxInput('district', label = "see detail number", value = FALSE)),
     
     mainPanel(plotOutput('N_film'))
   )
 )
+
 
 server <- function(input, output) {
   output$N_film <- renderPlot({
@@ -28,9 +29,9 @@ server <- function(input, output) {
                    'film in boxoffice' = release_type,
                    'unique film in boxoffice' = release_type_unique)
     X <- switch(input$var,
-                   'release film' = as.factor(release$year),
-                   'film in boxoffice' = as.factor(release_type$year),
-                   'unique film in boxoffice' = as.factor(release_type_unique$year))
+                   'release film' = release$year,
+                   'film in boxoffice' = release_type$year,
+                   'unique film in boxoffice' = release_type_unique$year)
     Y <- switch(input$var,
                     'release film' = release$N,
                     'film in boxoffice' = release_type$N,
@@ -39,12 +40,19 @@ server <- function(input, output) {
                    'release film' = release$type,
                    'film in boxoffice' = release_type$type,
                    'unique film in boxoffice' = release_type_unique$type)
-    data %>% ggplot(mapping = aes(x = X, y = Y, fill = Fill)) + geom_bar(stat = 'identity', position = 'dodge') + 
+    p <- data %>% ggplot(mapping = aes(x = X, y = Y, fill = Fill)) + geom_bar(stat = 'identity', position = 'dodge') + 
       scale_fill_brewer(palette = "Pastel1") + ylim(0, 140) + labs(x = 'Year', y = 'N') +
-      geom_text(aes(label = Y), vjust = 1.5, colour = "black", position = position_dodge(1), size = 5) +
       theme(axis.title = element_text(size = 13), axis.text = element_text(size = 13), legend.title = element_text(size = 13), legend.text = element_text(size = 12))
+    
+    label <-reactive({
+      if(input$district){
+        return(p + geom_text(aes(label = N), vjust = 1.5, colour = "black", position = position_dodge(1), size = 5))
+      }else{
+        return(p)
+      }
+    })
+    label()
   })
   }
-  
 
 shinyApp(ui = ui, server = server)
